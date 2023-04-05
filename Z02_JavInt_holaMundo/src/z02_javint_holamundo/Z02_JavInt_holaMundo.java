92b4faf60a9d9e78db0af723a5e2c84fee26949b
“Jhon <jhonfabrizio@gmail.com.com>
2023-04-04 20:50:01 -0500

    Publicando

---
+++ Z02_JavInt_holaMundo/src/z02_javint_holamundo/Z02_JavInt_holaMundo.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package z02_javint_holamundo;
+
+/**
+ *
+ * @author Maya
+ */
+public class Z02_JavInt_holaMundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Jhon");
+    }
+    
+}
